dc8f7ff950a7067e6260b636a36d8c5cbaba4654
SaranyaAnanthaRaman <saranyakumaar1996@gmail.com>
2023-11-21 13:17:27 +0530

    message

---
+++ src/org/phone/Phone.java
@@ -0,0 +1,26 @@
+package org.phone;
+public class Phone {
+	private void phoneInfo(String model) {
+		System.out.println("Model name is "+model);
+		this.phoneInfo(123465347659879l);
+		this.phoneInfo(16, 48);
+		this.phoneInfo(64);
+		this.phoneInfo("Qualcomm Snapdragon", "Blue");
+	}
+	private void phoneInfo(long imeino) {
+		System.out.println("imei no is "+imeino);
+	}
+	private void phoneInfo(int Camfront, int Camrear) {
+		System.out.println("Front cam is "+Camfront +"\n" +"Rear cam is "+Camrear);
+	}
+	private void phoneInfo(int storage) {
+		System.out.println("Storage is "+storage);
+	}
+	private void phoneInfo(String OS, String color) {
+		System.out.println("OS used "+OS +"\n" +"Color is "+color);
+	}
+public static void main(String[]args) {
+	Phone a = new Phone();
+	a.phoneInfo("Realme 5 pro");
+}
+}
